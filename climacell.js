c9f43bf5e28df5652bd18a957d98edd29af79c0a
Jose Romualdo Villalobos Perez <jorovipe@gmail.com>
2021-02-26 16:13:24 -0500

    Configured project

---
+++ climacell.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const fetch = require('node-fetch');
+
+const url = 'https://data.climacell.co/v4/timelines';
+
+console.log(process.version);
+
+
+const options = {method: 'GET'};
+const searchParams = new URLSearchParams({
+  // Medellin:
+  // google IBM's Temperature: 25 degrees.
+  // climacell Temperature: 17.33 degrees.
+  location: '6.44,-75.57',
+  fields: 'temperature',
+  timesteps: 'current',
+  units: 'metric',
+  timezone: 'America/Bogota',
+  apikey: process.env.CLIMA_CELL_API_KEY,
+})
+
+fetch(url + '?' + searchParams, options)
+  .then(response => response.json())
+  .then(json => console.log(JSON.stringify(json, undefined, 2)))
+  .catch(err => console.error(err));
